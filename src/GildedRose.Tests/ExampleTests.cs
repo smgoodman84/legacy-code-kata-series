a24b61aec8b5bcc55b73b7dfb14165c2978a72ab
Simon Goodman <simon.goodman@red-gate.com>
2016-03-01 13:29:26 +0000

    Test aged brie item

--- src/GildedRose.Tests/ExampleTests.cs
+++ src/GildedRose.Tests/ExampleTests.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GildedRose.Console;
 using NUnit.Framework;
 
 namespace GildedRose.Tests
@@ -5,9 +9,24 @@ namespace GildedRose.Tests
     public class ExampleTests
     {
         [Test]
-        public void TestTheTruth()
+        public void AgedBrieIncreasesInQuality()
         {
-            Assert.True(true);
+            var items = new List<Item>
+            {
+                new Item
+                {
+                    Name = "Aged Brie",
+                    Quality = 10,
+                    SellIn = 5
+                }
+            };
+
+            Program.UpdateQuality(items);
+
+            var item = items.First();
+            Assert.That(item.Name, Is.EqualTo("Aged Brie"));
+            Assert.That(item.Quality, Is.EqualTo(11));
+            Assert.That(item.SellIn, Is.EqualTo(4));
         }
     }
 }
